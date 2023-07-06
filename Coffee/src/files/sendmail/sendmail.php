aed6de2e75223671f0d96c2afdbf60e64f4a42ff
unknown <sirotuk93@gmail.com>
2023-07-06 15:25:49 +0300

    add done Coffee and add Educa with header and footer

--- Coffee/src/files/sendmail/sendmail.php
+++ Coffee/src/files/sendmail/sendmail.php
@@ -1,5 +1,6 @@
 <?php
 	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
 	use PHPMailer\PHPMailer\Exception;
 
 	require 'phpmailer/src/Exception.php';
@@ -11,20 +12,21 @@
 	$mail->setLanguage('ru', 'phpmailer/language/');
 	$mail->IsHTML(true);
 
-	/*
+
 	$mail->isSMTP();                                            //Send using SMTP
-	$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+	$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-	$mail->Username   = 'user@example.com';                     //SMTP username
-	$mail->Password   = 'secret';                               //SMTP password
+	$mail->Username   = 'sirotuk93@gmail.com';                     //SMTP username
+	$mail->Password   = 'erhergnchdpczdvs';                               //SMTP password
 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-	$mail->Port       = 465;                 
-	*/
+	$mail->Port       = 587;  
+	$mail->SMTPSecure = 'TLS'               
+
 
 	//Від кого лист
-	$mail->setFrom('from@gmail.com', 'Фрілансер по життю'); // Вказати потрібний E-mail
+	$mail->setFrom('sirotuk93@gmail.com', 'Фрілансер по життю'); // Вказати потрібний E-mail
 	//Кому відправити
-	$mail->addAddress('to@gmail.com'); // Вказати потрібний E-mail
+	$mail->addAddress('dsdevp@gmail.com'); // Вказати потрібний E-mail
 	//Тема листа
 	$mail->Subject = 'Вітання! Це "Фрілансер по життю"';
 
